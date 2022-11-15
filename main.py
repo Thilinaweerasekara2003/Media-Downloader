2dd5e03fb74976b89680de90e18a2bc151eddd24
PrabhashaOnline <89778323+PrabhashaOnline@users.noreply.github.com>
2022-11-15 00:33:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ async def ytdl_with_button(c: Client, message: Message):
 @bot.on_message(filters.command("start"))
 async def start(client, message):
     chat_id = message.chat.id
-    user_id = message.from_user["id"]
+    user_id = message.chat.id
     name = message.from_user["first_name"]
     if message.chat.type == "private":
         btn = InlineKeyboardMarkup(
